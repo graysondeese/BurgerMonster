7c321e953123d87ba02aa985d8a70833982c9126
Grayson Deese <gldeese@ncsu.edu>
2020-06-11 16:18:37 -0400

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-// Make connection.
+// Make the connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
